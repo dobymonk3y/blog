a7d4a82ac9f511c1dcb5bfe744c84e7f88f562b9
frankie <frankie@bengder.cc>
2016-06-30 10:15:42 +0800

    添加注册页面

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,27 @@
+@extends('app')
+
+@section('content')
+
+<div class="col-md-6 col-md-offset-3">
+    {!! Form::open(['url'=>'/auth/register']) !!}
+    <div class="form-group">
+	{!! Form::label('name','Name:') !!}
+	{!! Form::text('name',null,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+	{!! Form::label('email','Email:') !!}
+	{!! Form::email('email',null,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+	{!! Form::label('password','Password:') !!}
+	{!! Form::password('password',['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+	{!! Form::label('password_confirmation','Password_Confirm:') !!}
+        {!! Form::password('password_confirmation',['class'=>'form-control']) !!}
+    </div>
+    {!! Form::submit('Sing Up',['class'=>'btn btn-primary form-control']) !!}
+    {!! Form::close() !!}
+</div>
+
+@endsection

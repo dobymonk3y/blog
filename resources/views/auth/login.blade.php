b30bac31318c94e7eef143da7ab5fba338ff7c95
frankie <frankie@bengder.cc>
2016-06-30 09:38:07 +0800

    添加auth文件夹,新建login登陆页面

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,19 @@
+@extends('app')
+
+@section('content')
+
+<div class="col-md-4 col-md-offset-4">
+    {!! Form::open(['method'=>'post','url'=>'/auth/login']) !!}
+    <div class="form-group">
+	{!! Form::label('email','Email') !!}
+	{!! Form::email('email',null,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+	{!! Form::label('password','Password') !!}
+	{!! Form::password('password',['class'=>'form-control']) !!}
+    </div>
+    {!! Form::submit('Sing In',['class'=>'btn btn-primary form-control']) !!}
+    {!! Form::close() !!}
+</div>
+
+@endsection
